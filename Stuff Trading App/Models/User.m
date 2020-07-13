e92472df841a6957fbc6d0c772899338c06df99f
Ian Andre Aragon Saenz <ianaragon@fb.com>
2020-07-13 17:01:06 -0500

    added funtion to change profile pic

--- Stuff Trading App/Models/User.m
+++ Stuff Trading App/Models/User.m
@@ -16,4 +16,29 @@
     return (User *)[PFUser user];
 }
 
++ (void)setProfilePic:(UIImage *)image{
+    User *user = [User currentUser];
+    user.image = [self getPFFileFromImage:image];
+    [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+        if(succeeded){
+            NSLog(@"profile pic changed");
+        } else {
+            NSLog(@"pfile pic failed: %@", error.localizedDescription);
+        }
+    }];
+}
+
++ (PFFileObject *)getPFFileFromImage:(UIImage *_Nullable)image{
+   if (!image) {
+       return nil;
+   }
+   
+   NSData *imageData = UIImagePNGRepresentation(image);
+   if (!imageData) {
+       return nil;
+   }
+   
+   return [PFFileObject fileObjectWithName:@"image.png" data:imageData];
+}
+
 @end
