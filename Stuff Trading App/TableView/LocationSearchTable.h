d3bfdae466b803f160d57cfe7419f0f24ce7e7aa
Ian Andre Aragon Saenz <ianaragon@fb.com>
2020-07-20 15:06:51 -0500

    added table view controller for the search table

---
+++ Stuff Trading App/TableView/LocationSearchTable.h
@@ -0,0 +1,17 @@
+//
+//  LocationSearchTable.h
+//  Stuff Trading App
+//
+//  Created by Ian Andre Aragon Saenz on 20/07/20.
+//  Copyright © 2020 IanAragon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LocationSearchTable : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
