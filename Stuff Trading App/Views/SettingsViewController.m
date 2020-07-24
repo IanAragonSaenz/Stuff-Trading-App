2915fa7e52010209c0c099d4673ed9ec84f7dfa2
Ian Andre Aragon Saenz <ianaragon@fb.com>
2020-07-24 12:00:25 -0500

    changed functionality from settings to user profile

--- Stuff Trading App/Views/SettingsViewController.m
+++ Stuff Trading App/Views/SettingsViewController.m
@@ -31,65 +31,10 @@
         }
     }];
     
-    UITapGestureRecognizer *tapPic = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(takePhoto)];
-    [self.userPic addGestureRecognizer:tapPic];
-    [self.userPic setUserInteractionEnabled:YES];
+    
 }
 
-#pragma mark - Take Photo
 
-- (void)takePhoto {
-    UIImagePickerController *imagePC = [UIImagePickerController new];
-    imagePC.delegate = self;
-    imagePC.allowsEditing = YES;
-    
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Choose Media" message:@"Choose camera vs photo library" preferredStyle:(UIAlertControllerStyleActionSheet)];
-    UIAlertAction *camera = [UIAlertAction actionWithTitle:@"Camera" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
-            imagePC.sourceType = UIImagePickerControllerSourceTypeCamera;
-            [self presentViewController:imagePC animated:YES completion:nil];
-        } else {
-            [UIAlertController sendError:@"Camera source not found" onView:self];
-        }
-    }];
-    [alert addAction:camera];
-    
-    UIAlertAction *photoLibrary = [UIAlertAction actionWithTitle:@"Photo Library" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-        if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
-            imagePC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-            [self presentViewController:imagePC animated:YES completion:nil];
-        } else {
-            [UIAlertController sendError:@"Photo library source not found" onView:self];
-        }
-    }];
-    [alert addAction:photoLibrary];
-    
-    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
-    [alert addAction:cancel];
-    
-    [self presentViewController:alert animated:YES completion:nil];
-}
-
-- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
-    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
-    self.userPic.image = [self resizeImage:originalImage withSize:CGSizeMake(325, 325)];
-    [self dismissViewControllerAnimated:YES completion:nil];
-}
-
-#pragma mark - Sizing Image
-
-- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
-    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
-    
-    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
-    resizeImageView.image = image;
-    
-    UIGraphicsBeginImageContext(size);
-    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
-    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return newImage;
-}
 
 #pragma mark - Set Photo
 
--- Stuff Trading App/Views/UserViewController.m
+++ Stuff Trading App/Views/UserViewController.m
@@ -11,8 +11,9 @@
 #import "DetailPostViewController.h"
 #import "Chat.h"
 #import "MessageViewController.h"
+#import "UIAlertController+Utils.h"
 
-@interface UserViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+@interface UserViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIImageView *userImage;
 @property (weak, nonatomic) IBOutlet UILabel *usernameLabel;
@@ -20,6 +21,7 @@
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (weak, nonatomic) IBOutlet UIButton *messageButton;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *settingsButton;
+@property (weak, nonatomic) IBOutlet UIImageView *takePhotoImage;
 @property (strong, nonatomic) NSArray *posts;
 
 @end
@@ -32,15 +34,16 @@
     self.collectionView.delegate = self;
     self.collectionView.dataSource = self;
     
+    //sets poster spacing
     UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout *)self.collectionView.collectionViewLayout;
     layout.minimumInteritemSpacing = 2;
     layout.minimumLineSpacing = 2;
-    
     CGFloat posters = 2.0;
     CGFloat itemWidth = (self.collectionView.frame.size.width - layout.minimumInteritemSpacing * (layout.minimumLineSpacing)) / posters;
     CGFloat itemHeight = itemWidth;
     layout.itemSize = CGSizeMake(itemWidth, itemHeight);
     
+    //sets users info
     if(!self.user){
         self.user = [User currentUser];
     }
@@ -59,6 +62,14 @@
         if(!error)
             self.userImage.image = [UIImage imageWithData:data];
     }];
+    self.userImage.layer.cornerRadius = self.userImage.frame.size.width / 2;
+    self.takePhotoImage.layer.cornerRadius = self.takePhotoImage.frame.size.width / 2;
+    
+    //adds tap gesture ti take photo image
+    UITapGestureRecognizer *tapPic = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(takePhoto)];
+    [self.takePhotoImage addGestureRecognizer:tapPic];
+    [self.takePhotoImage setUserInteractionEnabled:YES];
+    
     [self fetchposts];
 }
 
@@ -116,6 +127,62 @@
             [self performSegueWithIdentifier:@"messageSegue" sender:chat];
         }
     }];
+}
+
+#pragma mark - Take Photo
+
+- (void)takePhoto {
+    UIImagePickerController *imagePC = [UIImagePickerController new];
+    imagePC.delegate = self;
+    imagePC.allowsEditing = YES;
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Choose Media" message:@"Choose camera vs photo library" preferredStyle:(UIAlertControllerStyleActionSheet)];
+    UIAlertAction *camera = [UIAlertAction actionWithTitle:@"Camera" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+            imagePC.sourceType = UIImagePickerControllerSourceTypeCamera;
+            [self presentViewController:imagePC animated:YES completion:nil];
+        } else {
+            [UIAlertController sendError:@"Camera source not found" onView:self];
+        }
+    }];
+    [alert addAction:camera];
+    
+    UIAlertAction *photoLibrary = [UIAlertAction actionWithTitle:@"Photo Library" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
+            imagePC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+            [self presentViewController:imagePC animated:YES completion:nil];
+        } else {
+            [UIAlertController sendError:@"Photo library source not found" onView:self];
+        }
+    }];
+    [alert addAction:photoLibrary];
+    
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+    [alert addAction:cancel];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    self.userImage.image = [self resizeImage:originalImage withSize:CGSizeMake(325, 325)];
+    [User setProfilePic:self.userImage.image];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - Sizing Image
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
 }
 
 #pragma mark - Navigation
