c4a0941455c513c5132725988cd60b5b7b93c1c8
Ian Andre Aragon Saenz <ianaragon@fb.com>
2020-07-27 17:47:30 -0500

    subclassed objects in order to add post picture and better segue to detail post

---
+++ Stuff Trading App/postAnnotation/MKPostAnnotation.m
@@ -0,0 +1,15 @@
+//
+//  MKPostAnnotation.m
+//  Stuff Trading App
+//
+//  Created by Ian Andre Aragon Saenz on 27/07/20.
+//  Copyright © 2020 IanAragon. All rights reserved.
+//
+
+#import "MKPostAnnotation.h"
+
+@implementation MKPostAnnotation
+
+@synthesize coordinate;
+
+@end
---
+++ Stuff Trading App/postAnnotation/PostPinAnnotationView.m
@@ -0,0 +1,22 @@
+//
+//  PostPinAnnotationView.m
+//  Stuff Trading App
+//
+//  Created by Ian Andre Aragon Saenz on 27/07/20.
+//  Copyright © 2020 IanAragon. All rights reserved.
+//
+
+#import "PostPinAnnotationView.h"
+
+@implementation PostPinAnnotationView
+
+@dynamic annotation;
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
