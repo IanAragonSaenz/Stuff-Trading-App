3bd7d47f8021c2c1a62dcf4c3206b38f67f23d52
Ian Andre Aragon Saenz <ianaragon@fb.com>
2020-07-29 16:36:05 -0500

    made the alert when taking picture to be in category

--- Stuff Trading App/Categories/UIAlertController+Utils.m
+++ Stuff Trading App/Categories/UIAlertController+Utils.m
@@ -19,4 +19,33 @@
     [view presentViewController:alert animated:YES completion:nil];
 }
 
++ (void)takePictureAlert:(UIViewController *)view withCompletion:(alertCompletion)completion {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Choose Media" message:@"Choose camera vs photo library" preferredStyle:(UIAlertControllerStyleActionSheet)];
+    UIAlertAction *camera = [UIAlertAction actionWithTitle:@"Camera" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+            completion(1);
+        } else {
+            [self sendError:@"Camera source not found" onView:view];
+            completion(0);
+        }
+    }];
+    [alert addAction:camera];
+    
+    UIAlertAction *photoLibrary = [UIAlertAction actionWithTitle:@"Photo Library" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
+            completion(2);
+        } else {
+            [self sendError:@"Photo library source not found" onView:view];
+            completion(0);
+        }
+    }];
+    [alert addAction:photoLibrary];
+    
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        completion(0);
+    }];
+    [alert addAction:cancel];
+    
+    [view presentViewController:alert animated:YES completion:nil];
+}
 @end
