9a22e40cd93aedd0497f55fe7931b832ca674468
Ian Andre Aragon Saenz <ianaragon@fb.com>
2020-07-31 10:11:10 -0500

    fixed collection view cells sizing in user view

--- Stuff Trading App/Views/UserViewController.m
+++ Stuff Trading App/Views/UserViewController.m
@@ -39,7 +39,7 @@
     layout.minimumInteritemSpacing = 2;
     layout.minimumLineSpacing = 2;
     CGFloat posters = 2.0;
-    CGFloat itemWidth = (self.collectionView.frame.size.width - layout.minimumInteritemSpacing * (layout.minimumLineSpacing)) / posters;
+    CGFloat itemWidth = (self.view.frame.size.width - layout.minimumInteritemSpacing * (layout.minimumLineSpacing)) / posters;
     CGFloat itemHeight = itemWidth;
     layout.itemSize = CGSizeMake(itemWidth, itemHeight);
     
