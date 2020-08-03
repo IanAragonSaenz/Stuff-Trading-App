468a053dde6743423ba22c6b0c20f165b4885f98
Ian Andre Aragon Saenz <ianaragon@fb.com>
2020-08-03 11:41:52 -0500

    fixed and if spacing, removed commented code in post annotations

--- Stuff Trading App/postAnnotation/PostPinAnnotationView.m
+++ Stuff Trading App/postAnnotation/PostPinAnnotationView.m
@@ -11,12 +11,5 @@
 @implementation PostPinAnnotationView
 
 @dynamic annotation;
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
 
 @end
